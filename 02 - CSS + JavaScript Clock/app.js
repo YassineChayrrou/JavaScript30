8c5e940b25e87362c7e58821908474eeaf25258b
YassineChayrrou <yassine.chayrrou@gmail.com>
2021-03-20 22:17:41 +0100

    working digital clock

---
+++ 02 - CSS + JavaScript Clock/app.js
@@ -0,0 +1,24 @@
+function realTimeClock() {
+  let now = new Date();
+  let minute = now.getMinutes();
+  let hour = now.getHours();
+  let second = now.getSeconds();
+
+  // check pm or am
+  let amOrPm = hour < 12 ? "AM" : "PM";
+
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+  hour = ("0" + hour).substr(-2);
+  minute = ("0" + minute).substr(-2);
+  second = ("0" + second).substr(-2);
+
+  // Display time
+
+  document.querySelector(
+    "#digital-clock"
+  ).innerHTML = `${hour} : ${minute} : ${second} ${amOrPm}`;
+
+  var t = setTimeout(realTimeClock, 500);
+}
